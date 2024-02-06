349046134cc55d688492ec66e16be46724a8f270
Eric Kennedy <erickennedy@rogers.com>
2024-02-06 17:16:27 +1300

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function app() {
 return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					kenneric.com
-				</p>
+		<div className="app">
+			<header className="app-header">
+				<div className="app-header-title rounded shadow-bottom">
+					<h1>
+						kenneric
+					</h1>
+				</div>
 			</header>
+			<div className="tab-menu">
+				<h2 className="rounded">
+					Clients
+				</h2>
+			</div>
+			<div className="container rounded shadow-bottom">
+				<div className="profile">
+					<div className="profile-name">
+						<h3>
+							Jack City
+						</h3>
+					</div>
+					<div className="profile-image rounded"></div>
+					<div className="audio-player"></div>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-export default App;
+export default app;
